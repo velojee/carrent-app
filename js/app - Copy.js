55fd21886857c351000d1421c89935409766400e
hvnplc <fadhlurrohmanfikri@gmail.com>
2024-09-05 21:15:48 +0700

    Sidebar kiri sudah okay dan ada hyperlink kalo butuh, Sidebar kanan tinggal nambah animasi, sisanya tinggal pusing pusingan lagi

--- js/app - Copy.js
+++ js/app - Copy.js
@@ -1,14 +1,11 @@
 let currentDropdown = null;
-
 // Function to toggle dropdown and close any previously opened dropdowns
 function toggleDropdown(id) {
     const dropdown = document.getElementById(id);
-
     // Close the currently open dropdown if it's different from the clicked one
     if (currentDropdown && currentDropdown !== dropdown) {
         currentDropdown.classList.remove('show');
     }
-
     // Toggle the clicked dropdown
     if (dropdown.classList.contains('show')) {
         dropdown.classList.remove('show');
@@ -25,10 +22,8 @@ function setActive(button) {
     document.querySelectorAll('.button-box button').forEach(btn => {
         btn.classList.remove('active');
     });
-
     // Add active class to the clicked button
     button.classList.add('active');
-
     // Update the main title to reflect the active button
     document.getElementById('main-title').textContent = `Rincian Transaksi: ${button.textContent}`;
 }
